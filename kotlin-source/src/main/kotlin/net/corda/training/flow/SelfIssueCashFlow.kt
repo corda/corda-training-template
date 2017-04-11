64a66dcd1f6a9af4223622e6581d8c0eaed53582
RogerWillis <roger.willis@r3cev.com>
2017-04-11 18:15:03 +0100

    Added self issue cash flow.

---
+++ kotlin-source/src/main/kotlin/net/corda/training/flow/SelfIssueCashFlow.kt
@@ -0,0 +1,23 @@
+package net.corda.training.flow
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.contracts.asset.Cash
+import net.corda.core.contracts.Amount
+import net.corda.flows.CashIssueFlow
+import net.corda.core.flows.FlowLogic
+import net.corda.core.serialization.OpaqueBytes
+import java.util.*
+
+class SelfIssueCashFlow(val amount: Amount<Currency>) : FlowLogic<Cash.State>() {
+    @Suspendable
+    override fun call(): Cash.State {
+        /** Create the cash issue command. */
+        val issueRef = OpaqueBytes.of(0)
+        val notary = serviceHub.networkMapCache.notaryNodes.single().notaryIdentity
+        val me = serviceHub.myInfo.legalIdentity
+        /** Create the cash issuance transaction. */
+        val cashIssueTransaction = subFlow(CashIssueFlow(amount, issueRef, me, notary))
+        /** Return the cash output. */
+        return cashIssueTransaction.tx.outputs.single().data as Cash.State
+    }
+}
