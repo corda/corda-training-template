b17a27a55f8fec371184beff9788186330815566
Hristo Gatsinski <h.gatsinski@magicsolutions.bg>
2018-08-13 17:12:00 +0300

    Fix wrong example request for `settleIOU` in `IOUApi`

--- kotlin-source/src/main/kotlin/net/corda/training/api/IOUApi.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/api/IOUApi.kt
@@ -159,7 +159,7 @@ class IOUApi(val rpcOps: CordaRPCOps) {
     /**
      * Settles an IOU. Requires cash in the right currency to be able to settle.
      * Example request:
-     * curl -X PUT 'http://localhost:10007/api/iou/issue-iou?amount=99&currency=GBP&party=O=ParticipantC,L=New%20York,C=US
+     * curl -X PUT 'http://localhost:10007/api/iou/settle-iou?amount=99&currency=GBP&id=08d9542c-cb24-4bfa-893f-94c8dcec00ef'
      */
     @GET
     @Path("settle-iou")
